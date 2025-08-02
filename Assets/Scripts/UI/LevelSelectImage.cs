e3050e56866ca3b419dbf826ea66c4720a4e736f
Andrik Kleine <andrik.k@web.de>
2025-08-02 18:12:47 +0200

    level select screen improved

--- Assets/Scripts/UI/LevelSelectImage.cs
+++ Assets/Scripts/UI/LevelSelectImage.cs
@@ -6,7 +6,7 @@ public class LevelSelectImage : MonoBehaviour
     public bool firstLevelScreen = false;
 
     private const int SCROLL_DISTANCE = 50;
-    private const float SCROLL_SPEED = 3f;
+    private const float SCROLL_SPEED = 500f;
 
     private float minY;
     private float maxY;
@@ -15,6 +15,16 @@ public class LevelSelectImage : MonoBehaviour
     {
         minY = Screen.height - GetComponent<RectTransform>().rect.height / 2 + SCROLL_DISTANCE;
         maxY = GetComponent<RectTransform>().rect.height / 2 - SCROLL_DISTANCE;
+
+        foreach (LevelButton levelButton in GetComponentsInChildren<LevelButton>())
+        {
+            if (levelButton.sceneName == SceneLoader.Instance().GetActiveSceneName())
+            {
+                // Center the button leading to the current scene
+                float targetY = Mathf.Clamp(Screen.height / 2 - levelButton.transform.localPosition.y, minY, maxY);
+                this.transform.position = new Vector3(this.transform.position.x, targetY, 0);
+            }
+        }
     }
 
     void Update()
@@ -24,10 +34,10 @@ public class LevelSelectImage : MonoBehaviour
 
         if (mouseY < SCROLL_DISTANCE && currentY < maxY)
         {
-            this.transform.position += new Vector3(0, SCROLL_SPEED * Time.time, 0);
+            this.transform.position += new Vector3(0, SCROLL_SPEED * Time.unscaledDeltaTime, 0);
         } else if (mouseY > Screen.height - SCROLL_DISTANCE && currentY > minY)
         {
-            this.transform.position -= new Vector3(0, SCROLL_SPEED * Time.time, 0);
+            this.transform.position -= new Vector3(0, SCROLL_SPEED * Time.unscaledDeltaTime, 0);
         }
     }
 }

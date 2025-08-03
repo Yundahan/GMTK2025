7ba5699d25a98bfb25a19f9df7cb6430d85df203
linovettel <lino.vettelschoss@web.de>
2025-08-03 12:27:58 +0200

    shadow moving anim

--- Assets/Scripts/ShadowActions.cs
+++ Assets/Scripts/ShadowActions.cs
@@ -7,6 +7,9 @@ public class ShadowActions : Interacter
     public Animator idleAnimator;
     public Animator movingAnimator;
 
+    public SpriteRenderer idleRenderer;
+    public SpriteRenderer movingRenderer;
+
     private bool isGrounded = true;
     private bool jumpBoosting = false;
 
@@ -14,6 +17,23 @@ public class ShadowActions : Interacter
     {
         Vector2 lastDirection = GetComponent<ShadowMovement>().GetLastDirection();
 
+        if (lastDirection.x > 0.01f)
+        { 
+             movingRenderer.flipX = false;
+             movingRenderer.enabled = true;
+             idleRenderer.enabled = false;
+         }
+        else if (lastDirection.x < -0.01f)
+        {
+            movingRenderer.flipX = true;
+            movingRenderer.enabled = true;
+            idleRenderer.enabled = false;
+        }
+        else 
+        {
+            movingRenderer.enabled = false;
+            idleRenderer.enabled = true;
+        }
         if (!isGrounded)
         {
             if (lastDirection.y > 0f && Mathf.Abs(lastDirection.x) > 0.01f)
@@ -32,12 +52,12 @@ public class ShadowActions : Interacter
         {
             if (Mathf.Abs(lastDirection.x) > 0.01f)
             {
-                movingAnimator.SetBool("isRunning", true);
+                idleAnimator.SetBool("isRunning", true);
                 // MOVING
             }
             else
             {
-                movingAnimator.SetBool("isRunning", false);
+                idleAnimator.SetBool("isRunning", false);
                 // IDLE
             }
         }
